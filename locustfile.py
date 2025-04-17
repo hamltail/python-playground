1bbc4982d440550a0db2e3de48640cbfef91f7c4
hamru <goo.hiro.w@gmail.com>
2025-04-17 18:02:56 +0900

    feat: Add wait_time to simulate user pacing

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,10 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
+import time
 
 class TestUser(HttpUser):
+    wait_time = between(1, 3)
+
     @task
     def get_test(self):
+        print(f"[{time.strftime('%H:%M:%S')}] sending request...")
         self.client.get('/get')
